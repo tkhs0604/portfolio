b12579b3c3a392662ac50eb6518a9022bb844b95
kenken <43767445+tkhs0604@users.noreply.github.com>
2021-07-05 01:22:17 +0900

    remove width and height properties from spacer

--- src/components/atoms/spacer.js
+++ src/components/atoms/spacer.js
@@ -4,9 +4,7 @@ export default function Spacer({ width, height }) {
   return (
     <div
       style={{
-        width: width,
         minWidth: width,
-        height: height,
         minHeight: height,
       }}
     />
